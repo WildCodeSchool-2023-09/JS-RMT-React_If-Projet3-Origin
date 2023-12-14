ab723849446ac4371ecea4c1a76fa4cdf1c35e81
Zonghero Jean Marc <jm.zonghero@gmail.com>
2023-12-14 16:26:11 +0100

    Création base de données SQL

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE
     vidéo (
         id INTEGER PRIMARY KEY INTEGER NOT NULL,
         title VARCHAR(255) NOT NULL,
-        définition VARCHAR(255) NOT NULL,
+        definition VARCHAR(255) NOT NULL,
         years INTEGER,
         url VARCHAR(255),
         FOREIGN key(title) REFERENCES category(id),
@@ -22,3 +22,42 @@ CREATE TABLE
         discipline VARCHAR(255),
         Foreign Key (title) REFERENCES video(id)
     );
+
+INSERT INTO category(category)
+VALUES('Athlétisme'),
+INSERT INTO
+    category(category)
+VALUES('Natation'),
+INSERT INTO
+    category(category)
+VALUES('Football'),
+INSERT INTO
+    category(category)
+VALUES('Velo'),
+INSERT INTO
+    category(category)
+VALUES('Ski'),
+INSERT INTO
+    category(category)
+VALUES('Rally'),
+INSERT INTO
+    category(category)
+VALUES('Rally'),
+INSERT INTO
+    category(category)
+VALUES('Formula one'),
+INSERT INTO
+    category(category)
+VALUES('E-sport'),
+INSERT INTO
+    category(category)
+VALUES('E-sport');
+
+INSERT INTO
+    video(title, definition, years, url)
+VALUES
+(
+        'Ben Johnson Vs Carl Lewis, 1988 Olympics 100m Semi-Finals Ben Johnson Carl Lewis CBC',
+        1988,
+        'https://youtu.be/Wcya6ccGfVg?si=FvbI5EobIh5Nb2My'
+    ),
