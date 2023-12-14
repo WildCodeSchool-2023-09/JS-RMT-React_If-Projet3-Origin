a4f0e5c13349d2b4c18f9ef197d5b3c710edb688
Zonghero Jean Marc <jm.zonghero@gmail.com>
2023-12-14 16:00:22 +0100

    création tables SQL

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,4 +1,24 @@
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null
-);
+CREATE TABLE
+    category (
+        id int primary key auto_increment not null,
+        category VARCHAR(255) NOT NULL,
+    );
+
+CREATE TABLE
+    vidéo (
+        id INTEGER PRIMARY KEY INTEGER NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        définition VARCHAR(255) NOT NULL,
+        years INTEGER,
+        url VARCHAR(255),
+        FOREIGN key(title) REFERENCES category(id),
+    );
+
+CREATE TABLE
+    sport (
+        id INTEGER PRIMARY KEY INTEGER NOT NULL,
+        label VARCHAR(255),
+        type VARCHAR(255),
+        discipline VARCHAR(255),
+        Foreign Key (title) REFERENCES video(id)
+    );
