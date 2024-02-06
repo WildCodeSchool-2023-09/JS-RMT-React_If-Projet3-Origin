e09f9339918de390033842afcae07075630c6875
alain le moal <wolfdu28@aol.com>
2024-02-06 14:09:24 +0100

    modification du css page contact video inscption avec modif insciptorigin jsx

--- frontend/src/pages/admin/InscriptOrigins.jsx
+++ frontend/src/pages/admin/InscriptOrigins.jsx
@@ -29,7 +29,7 @@ function InscriptOrigins() {
             <input type="password" name="password" required />
           </label>
           <label>
-            Confirmation du mot de passe
+            Confirmation
             <input type="password" name="comfirmedPassword" required />
           </label>
           <button className="Inscription" type="submit">
